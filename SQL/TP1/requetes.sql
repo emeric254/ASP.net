ee185188a383142fe56cf1d7236122110bb40cd4
emeric254 <emeric254@live.fr>
2015-10-01 11:01:22 +0200

    mise en forme tp1, sceance 2 chiante ...

--- SQL/TP1/requetes.sql
+++ SQL/TP1/requetes.sql
@@ -1,5 +1,5 @@
---Q5)a)
---Affichage du contenu de chaque table
+-- Q5)a)
+-- Affichage du contenu de chaque table
 
 SELECT [Id_cat]
       ,[Nom_cat]
@@ -48,14 +48,16 @@ SELECT [Id_client]
       ,[Email_client]
   FROM [stri16].[dbo].[CLIENT]
 ;
---b)
+-- b)
+-- Affichage des produits en promotion
 
 SELECT [Titre_prod]
       ,[Detail_prod]
       ,[Prixhtprom_prod]
   FROM [stri16].[dbo].[PRODUIT] WHERE [Prom_prod]=1
 ;
---c)
+-- c)
+-- Affichage de  'nom cat' pour chaque produit
 
 SELECT [Titre_prod]
       ,[stri16].[dbo].[CATEGORIE].[Nom_cat]
@@ -64,7 +66,8 @@ SELECT [Titre_prod]
 	FROM [stri16].[dbo].[PRODUIT], [stri16].[dbo].[CATEGORIE]
 	WHERE [stri16].[dbo].[CATEGORIE].[Id_cat]=[stri16].[dbo].[PRODUIT].[Id_cat] AND [Prom_prod]=1
 ;
---d)
+-- d)
+-- Affichage des produits appartenant à une catégorie dont le 'nom_cat' est donné
 SELECT [Titre_prod]
       ,[stri16].[dbo].[CATEGORIE].[Nom_cat]
       ,[Detail_prod]
@@ -72,7 +75,8 @@ SELECT [Titre_prod]
 	FROM [stri16].[dbo].[PRODUIT], [stri16].[dbo].[CATEGORIE]
 	WHERE [stri16].[dbo].[CATEGORIE].[Id_cat]=[stri16].[dbo].[PRODUIT].[Id_cat] AND [stri16].[dbo].[CATEGORIE].[Nom_cat] = 'Imprimantes'
 ;
---e)
+-- e)
+-- Affichage des commandes d'un client don le 'nom_client' est donné
 
 SELECT [stri16].[dbo].[COMMANDE].[Id_com]
       ,[stri16].[dbo].[COMMANDE].[Date_com]
@@ -81,7 +85,8 @@ SELECT [stri16].[dbo].[COMMANDE].[Id_com]
       ,[stri16].[dbo].[DETAIL_COMMANDE].[Qt_dc]
   FROM [stri16].[dbo].[COMMANDE], [stri16].[dbo].[DETAIL_COMMANDE] WHERE [stri16].[dbo].[COMMANDE].[Id_client]=2 AND [stri16].[dbo].[DETAIL_COMMANDE].[Id_com] = [stri16].[dbo].[COMMANDE].[Id_com] ORDER BY [stri16].[dbo].[COMMANDE].[Id_com]
 ;
---f)
+-- f)
+-- Affichage des clients ayant commandé le produit dont le 'titre_prod' est donné
 
 SELECT [stri16].[dbo].[CLIENT].[Id_client]
       ,[stri16].[dbo].[CLIENT].[Nom_client]
@@ -94,7 +99,9 @@ AND [stri16].[dbo].[DETAIL_COMMANDE].[Id_com] =  [stri16].[dbo].[COMMANDE].[Id_c
 AND [stri16].[dbo].[COMMANDE].[Id_client] = [stri16].[dbo].[CLIENT].[Id_client]
 ;
 
+
 -- 6)
+-- Insertion d'un nouveau client
 INSERT INTO [stri16].[dbo].[CLIENT]
            ([Nom_client]
            ,[Prenom_client]
@@ -117,7 +124,11 @@ INSERT INTO [stri16].[dbo].[CLIENT]
            ,'C@stri.fr')
 ;
 
---7)
+-- 7)
+-- Nouvelle commande de Jean Marc
+
+DECLARE @idClient int;
+SELECT @idClient = @@identity;
 
 INSERT INTO [stri16].[dbo].[COMMANDE]
            ([Date_com]
@@ -128,14 +139,18 @@ INSERT INTO [stri16].[dbo].[COMMANDE]
            ,[Total_com])
      VALUES
            ('15/01/03'
-           ,4
+           ,@idClient
            ,'16/01/03'
            ,'CH'
            ,'cheque'
            ,400)
 ;
 
---8)
+-- 8)
+-- Insertion détail de la commande
+
+DECLARE @idCommande int;
+SELECT @idCommande = @@identity;
 
 INSERT INTO [stri16].[dbo].[DETAIL_COMMANDE]
            ([Id_prod]
@@ -144,23 +159,31 @@ INSERT INTO [stri16].[dbo].[DETAIL_COMMANDE]
            ,[Prix_dc])
      VALUES
            ('Imp_HP'
-           ,4
+           ,@idCommande
            ,2
            ,200)
 ;
 
---9)
+-- 9)
+-- Affichage du client ayant fait le plus de commande
 
-SELECT TOP 1 l.[Id_client], COUNT(c.[Id_com]) as 'lol'
+SELECT TOP 1 l.[Id_client],
+			(select u.[Nom_Client] from [stri16].[dbo].[CLIENT] u where l.[Id_client] = u.[Id_client]) as 'NomClient',
+			(select u.[Prenom_Client] from [stri16].[dbo].[CLIENT] u where l.[Id_client] = u.[Id_client]) as 'PrenomClient',
+			COUNT(c.[Id_com]) as 'NbCommandes'
 	FROM [stri16].[dbo].[COMMANDE] c, [stri16].[dbo].[CLIENT] l
 	WHERE c.[Id_client] = l.[Id_client]
-	GROUP BY l.[Id_client] ORDER BY lol desc
+	GROUP BY l.[Id_client] ORDER BY NbCommandes desc
 ;
 
 -- 10) 
+-- Affichage du client dont la commande comporte le plus de produits
 
-SELECT TOP 1 l.[Id_client], COUNT(d.[Qt_dc]) as 'lol'
+SELECT TOP 1 l.[Id_client],
+			(select u.[Nom_Client] from [stri16].[dbo].[CLIENT] u where l.[Id_client] = u.[Id_client]) as 'NomClient',
+			(select u.[Prenom_Client] from [stri16].[dbo].[CLIENT] u where l.[Id_client] = u.[Id_client]) as 'PrenomClient',
+			COUNT(d.[Qt_dc]) as 'NbProduitsCommandes'
 	FROM [stri16].[dbo].[COMMANDE] c, [stri16].[dbo].[CLIENT] l, [stri16].[dbo].[DETAIL_COMMANDE] d
 	WHERE c.[Id_client] = l.[Id_client] AND c.[Id_com] = d.[Id_com]
-	GROUP BY d.[Id_com], l.[Id_client] ORDER BY lol desc
+	GROUP BY d.[Id_com], l.[Id_client] ORDER BY NbProduitsCommandes desc
 ;
